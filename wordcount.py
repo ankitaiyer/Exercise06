536870e27ac32dda1cc0616a648f6602437e08a5
Hackbright Student <user@mini-002.(none)>
2014-02-10 15:16:27 -0800

    extra cred, stripped punctuation and merged 2 for loops to 1

--- wordcount.py
+++ wordcount.py
@@ -1,26 +1,29 @@
 
 import pprint
 
-inputfile = open("twain.txt")
+inputfile = open("medium.txt")
 filecontents = inputfile.read()
 
-wordlist = filecontents.lower().split()
-#print wordslist
+before_wordlist = filecontents.lower().split()
+
+wordlist = []
+for word in before_wordlist:
+    newword = word.strip(",.:?!#*()[]%@$^&")
+    wordlist.append(newword)
+
+
 
 word_dict = {}
 
-# setup a dictionary of all the words, and set the value to 0
-for i in wordlist:
-    if i not in word_dict:
-        word_dict[i] =0
+#go thru wordlist, if increment the value if the word exists,
+#else if word is not in the dict, add it as a key, default value of 1
 
+for word in wordlist:
+    if word in word_dict:
+        word_dict[word]+= 1
+    else:
+        word_dict.setdefault(word,1)
 
-# now go thru each word in the dict we made, 
-# and increment every time we find it in wordlist
-for i in wordlist:
-    if i in word_dict:
-        word_dict[i]+= 1
-        #print i, word_dict[i]
 
 pprint.pprint(word_dict)
     
